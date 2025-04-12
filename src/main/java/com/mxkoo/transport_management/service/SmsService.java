2507b27419dcc5911a16aa7c60d4bfad8305ca58
darchala <damianarchala@gmail.com>
2025-04-12 21:34:45 +0200

    TASK-8: Migrate application.properties to application.yml, add TwilioConfiguration

--- src/main/java/com/mxkoo/transport_management/service/SmsService.java
+++ src/main/java/com/mxkoo/transport_management/service/SmsService.java
@@ -3,12 +3,9 @@ package com.mxkoo.transport_management.service;
 import com.mxkoo.transport_management.entity.Driver;
 import com.mxkoo.transport_management.entity.Road;
 import com.mxkoo.transport_management.repository.RoadRepository;
-import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -16,27 +13,18 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
-@Service
-@PropertySource("classpath:application.properties")
-public class SmsService{
-    private RoadRepository roadRepository;
-    @Value("${twilio.account_sid}")
-    private String accountSid;
 
-    @Value("${twilio.auth_token}")
-    private String authToken;
-    public SmsService(RoadRepository roadRepository) {
-        this.roadRepository = roadRepository;
-    }
-    @PostConstruct
-    public void initTwilio() {
-        Twilio.init(accountSid, authToken);
-    }
+@Service
+@RequiredArgsConstructor
+public class SmsService {
+
+    private final RoadRepository roadRepository;
+
     @Scheduled(cron = "0 0 0 * * *")
-    public void checkRoads(){
+    public void checkRoads() {
         List<Road> roads = roadRepository.findAll();
         for (Road road : roads) {
-            if (ChronoUnit.DAYS.between(LocalDate.now(), road.getDepartureDate()) == 1){
+            if (ChronoUnit.DAYS.between(LocalDate.now(), road.getDepartureDate()) == 1) {
                 createAndSendSMS(road.getDriver());
             }
         }
@@ -44,16 +32,18 @@ public class SmsService{
     }
 
     private void createAndSendSMS(Driver driver) {
-        String recipientNumber = driver.getContactNumber().toString();
+        String recipientNumber = driver.getContactNumber()
+                                       .toString();
         String twilioNumber = System.getenv("TWILIO_NUMBER");
         Optional<Road> roadOptional = roadRepository.findFirstByDriverIdOrderByDepartureDateAsc(driver.getId());
         if (roadOptional.isPresent()) {
-            String destination = roadOptional.get().getTo();
+            String destination = roadOptional.get()
+                                             .getTo();
             Message.creator(
-                            new PhoneNumber(recipientNumber),
-                            new PhoneNumber(twilioNumber),
-                            "Został 1 dzień do trasy do " + destination)
-                    .create();
+                           new PhoneNumber(recipientNumber),
+                           new PhoneNumber(twilioNumber),
+                           "Został 1 dzień do trasy do " + destination)
+                   .create();
         }
 
     }
